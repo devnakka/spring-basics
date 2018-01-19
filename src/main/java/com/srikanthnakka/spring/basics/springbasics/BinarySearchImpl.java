ebd9cd5b2bc3a9c5ceec5bb73baa478fa8241ca9
Srikanth Nakka <srikanthnakka@gmail.com>
2018-01-20 01:17:23 +0530

    @Qualifier > @Primary > ByName > ByType

--- src/main/java/com/srikanthnakka/spring/basics/springbasics/BinarySearchImpl.java
+++ src/main/java/com/srikanthnakka/spring/basics/springbasics/BinarySearchImpl.java
@@ -4,17 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+
+// @Qualifier > @Primary > ByName > ByType
 @Component
 public class BinarySearchImpl {
 	
 	@Autowired
-	@Qualifier("bubble")
-	private SortAlgorithm sortAlgorithm;
+//	@Qualifier("bubble")
+	private SortAlgorithm bubbleSortAlgorithm;
 	
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 
-		int[] sortedNumbers = sortAlgorithm.sort(numbers);
-		System.out.println(sortAlgorithm);
+		int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+		System.out.println(bubbleSortAlgorithm);
 		// Search the array
 		return 3;
 	}
